9456caf10c719cfc362060b9bbd14ab6bf0f59c8
Moses Fawole <mosesfawole2001@gmail.com>
2022-07-20 14:11:31 +0100

    counter app with redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/index";
+import { act } from "react-dom/test-utils";
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   const increment = () => {
-    dispatch({ type: "INC" });
+    dispatch(actions.increment());
   };
   const decrement = () => {
-    dispatch({ type: "DEC" });
+    dispatch(actions.decrement());
   };
   const addBy = () => {
-    dispatch({ type: "ADD", payload: 10 });
+    dispatch(actions.addBy(10));
   };
   return (
     <div>
