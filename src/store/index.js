f75c9d9be96dd6df7cf22393edd47da7b96e7cf8
Moses Fawole <mosesfawole2001@gmail.com>
2022-07-20 13:57:14 +0100

    install reduxjs@toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,25 @@
 import { createStore } from "redux";
 
 const reducerFn = (state = { counter: 0 }, action) => {
+  // Synchronous function
+  // We should not mutate the original state
+
+  if (action.type == "INC") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DEC") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  if (action.type == "ADD") {
+    return {
+      counter: state.counter + action.payload,
+    };
+  }
+
   return state;
 };
 
